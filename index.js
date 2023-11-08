66bac18f1843b151fff87377d8144aa3b2aabc7b
Airin Rahat Prome <airinrahatprome@gmail.com>
2023-11-08 15:41:48 +0600

    added env file

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 console.log(process.env.DB_PASS);
@@ -25,11 +31,55 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log("token in the middleware", token);
+
+  //no token avaible
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+  // next();
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const JobCollection = client.db("jobWorldDB").collection("addjob");
+    const AppliedJobCollection = client
+      .db("jobWorldDB")
+      .collection("appliedjob");
+
+    // jwt
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("user for token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("logging out", user);
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
 
     //my job data get
     app.get("/addjob", async (req, res) => {
@@ -46,17 +96,61 @@ async function run() {
       const result = await JobCollection.findOne(query);
       res.send(result);
     });
-    //my job data get emaill
-    app.get("/addjob", async (req, res) => {
-      console.log(req.query);
-      const result = await JobCollection.insertOne();
+
+    //my job data category gat
+
+    app.get("/alljobscategory", async (req, res) => {
+      console.log(req.query.jobcategory);
+      let query = {};
+      if (req.query?.jobcategory) {
+        query = { jobcategory: req.query.jobcategory };
+      }
+      const result = await JobCollection.find(query).toArray();
       res.send(result);
     });
+
+    //my job data get emaill
+    app.get("/myjobs", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await JobCollection.find(query).toArray();
+      res.send(result);
+    });
+
     //my job data post
     app.post("/addjob", async (req, res) => {
       const newaddJob = req.body;
       console.log(newaddJob);
       const result = await JobCollection.insertOne(newaddJob);
+      res.send(result);
+    });
+
+    //applied job data post
+    app.post("/appliedjob", async (req, res) => {
+      const newappliedJob = req.body;
+      console.log(newappliedJob);
+      const result = await AppliedJobCollection.insertOne(newappliedJob);
+      res.send(result);
+    });
+
+    //applied job data get
+    app.get("/appliedjob", async (req, res) => {
+      const cursor = AppliedJobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //applied job data get email
+    app.get("/myappliedjobs", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await AppliedJobCollection.find(query).toArray();
       res.send(result);
     });
 
@@ -77,6 +171,7 @@ async function run() {
           photo: updatedJob.photo,
           description: updatedJob.description,
           applicationDeadline: updatedJob.applicationDeadline,
+          JobApplicant: updatedJob.JobApplicant,
         },
       };
       const result = await JobCollection.updateOne(filter, job, options);
